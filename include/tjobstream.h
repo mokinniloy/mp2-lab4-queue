b27d01f132b185f7565d404d10a37b31e59828e6
Сергей Коннов <sergeyzorro007@gmail.com>
2018-12-08 13:34:42 +0300

    stack and queue done

---
+++ include/tjobstream.h
@@ -0,0 +1,6 @@
+#ifndef __JOBSTREAM_H__
+#define __JOBSTREAM_H__
+
+
+
+#endif // __JOBSTREAM_H__
---
+++ include/tqueue.h
@@ -0,0 +1,20 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#include "tstack.h"
+
+class TQueue: public TStack
+{
+private:
+    int Li;
+public:
+    TQueue(int Size = DefMemSize);
+    ~TQueue() {};
+    TData Get();
+    TData TopElem();
+    void Print();
+private:
+    int GetNextIndex(int ind);
+};
+
+#endif // __QUEUE_H__
