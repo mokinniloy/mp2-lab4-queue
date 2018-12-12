bfacfbeff647fe35bb1f628bd6bbd369929ace63
Zhivaev Artem <zhivaev993@gmail.com>
2018-12-13 00:50:32 +0300

    tstack.cpp Added

--- include/tstack.h
+++ include/tstack.h
@@ -6,16 +6,16 @@
 class TStack :public TDataRoot
 {
 private:
-	int top; 
+  int top; 
 public:
-    TStack(int Size = DefMemSize);
-    ~TStack() {};
-	void Put(const TData &Val); 
-	TData Get();
-	virtual TData TopElem(); 
+  TStack(int Size = DefMemSize);
+  ~TStack() {};
+  void Put(const TData &Val); 
+  TData Get();
+  virtual TData TopElem(); 
 
-	int IsValid();
-    void Print();
+  int IsValid();
+  void Print();
 };
 
 #endif
---
+++ src/tstack.cpp
@@ -0,0 +1,55 @@
+#include "tstack.cpp"
+#include <iostream>
+
+TStack::TStack(int Size) : TDataRoot(Size), top(-1) {}
+
+void TStack:: Put(const TData &Val)
+{
+  if(TDataRoot::IsFull())
+    throw DataFull;
+
+  TDataRoot::DataCount++;
+
+  if(TDataRoot::IsFull())
+    TDataCom::SetRetCode(DataFull);
+  else
+    TDataRoot::SetRetCode(DataOK);
+
+  TDataRoot::pMem[++top] = Val;
+}
+
+TData TStack::Get()
+{
+  if(TDataRoot::IsEmpty())
+    throw DataEmpty;
+
+  TDataRoot::DataCount--;
+
+  if(TDataRoot::IsEmpty())
+    TDataCom::SetRetCode(DataEmpty);
+  else
+    TDataRoot::SetRetCode(DataOK);
+
+  return TDataRoot::pMem[top--];
+}
+
+TData TStack::TopElem()
+{
+  if(TDataRoot::IsEmpty)
+    throw DataEmpty;
+
+  return TDataRoot::pMem[top];
+}
+
+int TStack::IsValid()
+{
+  return 1;
+}
+
+void Print()
+{
+  std::cout << "{ ";
+  for(int i = 0; i < top; ++i)
+    cout << TDataRoot::pMem[i] << "; ";
+  std::cout << '}';
+}
