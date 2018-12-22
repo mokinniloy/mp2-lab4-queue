0748fc00db6725e0ed739071ac877199f501f5ef
Татьяна Кондрина <kondrina.tanya@gmail.com>
2018-12-22 23:21:45 +0300

    Something done, but I dont know what exactly

---
+++ src/tproc.cpp
@@ -0,0 +1,23 @@
+#include "tjobstream.h"
+#include <iostream>
+
+TJobStream::TJobStream(double _q1)
+{
+	q1 = _q1;
+	mt = std::mt19937(time(0));
+	rand = std::uniform_real_distribution<double>(0, 1);
+}
+
+bool TJobStream::IsTask()
+{
+	if (rand(mt) <= q1)
+		return 1;
+	else
+		return 0;
+}
+
+
+int TJobStream::GetTask()
+{
+	return cnt++;
+}
