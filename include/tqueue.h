04e3800d521b32a2ebfefe80f74077001de47b6a
RoStroen <roman.strelczov14@mail.ru>
2018-12-23 22:46:34 +0300

    laba4

---
+++ include/tqueue.h
@@ -0,0 +1,22 @@
+
+  #ifndef __QUEUE_H__
+  #define __QUEUE_H__
+  
+   #include "tstack.h"
+  
+   class TQueue: public TStack
+  {
+  private:
+      int Li;
+  public:
+      TQueue(int Size = DefMemSize);
+      ~TQueue() {};
+      TData Get();
+      TData TopElem();
+      void Print();
+      int GetSize() const;
+  private:
+      int GetNextIndex(int ind);
+  };
+  
+   #endif // __QUEUE_H__
---
+++ samples/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+  #include "tjobstream.h"
+  #include "tproc.h"
+  #include "tqueue.h"
+  
+   using namespace std;
+  
+   int main()
+  {
+      TJobStream task;
+      TProc proc;
+      int NumTasks = 0, NumComplete = 0, NumRejections = 0, MediumTime = 0, NumEmpty = 0, UnprocessedTasks = 0;
+      int Tact = 1000000;
+      int QSize = 10;
+      TQueue q(QSize);
+      for(int i = 0; i < Tact; i++)
+      {
+          if(task.IsTask())
+          {
+              ++NumTasks;
+              int tmp1 = task.GetTask();
+              if(q.IsFull())
+                  ++NumRejections;
+              else
+                  q.Put(tmp1);
+          }
+          if(proc.IsEmpty())
+          {
+              if(q.IsEmpty())
+                  ++NumEmpty;
+              else
+              {
+                  q.Get();
+                  proc.Busy();
+              }
+          }
+          else
+          {
+              if(proc.IsComplete())
+              {
+                  ++NumComplete;
+                  proc.Ready();
+                  if(q.IsEmpty())
+                      ++NumEmpty;
+                  else
+                  {
+                      q.Get();
+                      proc.Busy();
+                  }
+              }
+          }
+      }
+  
+       UnprocessedTasks = q.GetSize();
+      if(!proc.IsEmpty())
+          ++UnprocessedTasks;
+      cout<<"Number of tasks: "<<NumTasks<<"\n";
+      cout<<"Number of rejections: "<<double(NumRejections)*100/double(NumTasks)<<"%\n";
+      cout<<"Medium time on one task: "<<double(Tact-NumEmpty)/double(NumComplete)<<"\n";
+      cout<<"Number of idle tacts: "<<double(NumEmpty)*100/double(Tact)<<"%\n";
+  
+       return 0;
+  }
